9c31ccd1c2da19c6267a33459afc36c0d1ec9ab6
Hervinder <hbhandal80@hotmail.com>
2021-05-20 19:53:14 +0000

    Fixed error with dynamic tweets. Now appearing and correctly formatted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -28,34 +28,48 @@ $(document).ready(function() {
         },
         "created_at": 1621391971665
       }
-    ]
-});
+    ];
 
-const renderTweets = function(tweets) {
-for (let tweet of tweets) {
-  const $tweet = createTweetElement(tweet);
-  $('.tweetsSection').prepend($tweet);
-}
-};
+    
+    const createTweetElement = function(tweet) {
+      const $tweet = $(`<article id="old-tweet"></article>`);
+      const user = tweet.user;
+      const content = tweet.content;
+      const tweetMarkup = `
+      <div id="user">
+        <img id="avatar" src=${user.avatars}></img>
+        <label id="users-name">${user.name}</label>
+        <label id="username">${user.handle}</label>
+      </div>
+      <div id="tweet">
+        <label id="tweeted">${content.text}</label>
+      </div>
+      <footer>
+      <div>
+        <span id="time">${tweet.created_at} days ago</span>
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+      <br>
+      <br>
+      `;
+      
+      $tweet.append(tweetMarkup);
 
-const createTweetElement = function(tweet) {
-const tweetMarkup = `
-  <div id="user">
-    <img id="avatar">${user.avatars}</img>
-    <label id="users-name">${user.name}</label>
-    <label id="username">${user.handle}</label>
-  </div>
-  <div id="tweet">
-    <label id="tweeted">${content.text}</label>
-  </div>
-  <div>
-    <span id="time">${date}days ago</span>
-    <i class="fas fa-flag"></i>
-    <i class="fas fa-retweet"></i>
-    <i class="fas fa-heart"></i>
-  </div>
-`;
-return $tweet;
-}
+      console.log($tweet); // to see what it looks like
+      //$('#tweets-container').append($tweet);
+      return $tweet;
+    }
 
-renderTweets(data);
+    const renderTweets = function(tweets) {
+      for (let tweet of tweets) {
+        const $tweet = createTweetElement(tweet);
+        $('.tweetsSection').prepend($tweet);
+      }
+    };
+    
+    renderTweets(data);
+    
+  });
+
